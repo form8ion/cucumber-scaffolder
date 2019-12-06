e640b96a8fe48c3922b0c45f94e51b1ce3249b37
Matt Travi <programmer@travi.org>
2019-12-05 22:52:10 -0600

    feat(scaffolder): extracted existing logic from javascript-scaffolder
    https://github.com/travi/javascript-scaffolder/blob/7e6969e3eff14514b92dc77577ff46cbcff98740/src/testing/cucumber.js

---
+++ src/scaffolder-test.js
@@ -0,0 +1,51 @@
+import {promises as fsPromises} from 'fs';
+import {assert} from 'chai';
+import any from '@travi/any';
+import sinon from 'sinon';
+import scaffoldCucumber from './scaffolder';
+
+suite('cucumber scaffolder', () => {
+  let sandbox;
+  const projectRoot = any.string();
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(fsPromises, 'copyFile');
+    sandbox.stub(fsPromises, 'writeFile');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that cucumber is scaffolded', async () => {
+    fsPromises.copyFile.resolves();
+    fsPromises.writeFile.resolves();
+
+    assert.deepEqual(
+      await scaffoldCucumber({projectRoot}),
+      {
+        devDependencies: ['cucumber', 'chai', 'gherkin-lint'],
+        scripts: {
+          'lint:gherkin': 'gherkin-lint',
+          'test:integration': 'run-s \'test:integration:base -- --profile noWip\'',
+          'test:integration:base': 'DEBUG=any cucumber-js test/integration --profile base',
+          'test:integration:debug': 'DEBUG=test run-s test:integration',
+          'test:integration:wip': 'run-s \'test:integration:base -- --profile wip\'',
+          'test:integration:focus': 'run-s \'test:integration:base -- --profile focus\''
+        },
+        eslintConfigs: ['cucumber']
+      }
+    );
+    assert.calledWith(fsPromises.copyFile, '../templates/cucumber.txt', `${projectRoot}/cucumber.js`);
+    assert.calledWith(
+      fsPromises.writeFile,
+      `${projectRoot}/.gherkin-lintrc`,
+      JSON.stringify({
+        'no-restricted-tags': ['on', {tags: ['@focus']}],
+        'use-and': 'on',
+        'no-multiple-empty-lines': 'on',
+        'no-dupe-feature-names': 'on'
+      })
+    );
+  });
+});
---
+++ src/scaffolder.js
@@ -0,0 +1,29 @@
+import {promises as fsPromises} from 'fs';
+
+export default async function ({projectRoot}) {
+  await Promise.all([
+    fsPromises.copyFile('../templates/cucumber.txt', `${projectRoot}/cucumber.js`),
+    fsPromises.writeFile(
+      `${projectRoot}/.gherkin-lintrc`,
+      JSON.stringify({
+        'no-restricted-tags': ['on', {tags: ['@focus']}],
+        'use-and': 'on',
+        'no-multiple-empty-lines': 'on',
+        'no-dupe-feature-names': 'on'
+      })
+    )
+  ]);
+
+  return {
+    devDependencies: ['cucumber', 'chai', 'gherkin-lint'],
+    scripts: {
+      'lint:gherkin': 'gherkin-lint',
+      'test:integration': 'run-s \'test:integration:base -- --profile noWip\'',
+      'test:integration:base': 'DEBUG=any cucumber-js test/integration --profile base',
+      'test:integration:debug': 'DEBUG=test run-s test:integration',
+      'test:integration:wip': 'run-s \'test:integration:base -- --profile wip\'',
+      'test:integration:focus': 'run-s \'test:integration:base -- --profile focus\''
+    },
+    eslintConfigs: ['cucumber']
+  };
+}

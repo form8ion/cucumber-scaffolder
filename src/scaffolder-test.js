fe2a5289210da2c897409e112f862a5f6f9205d5
Matt Travi <programmer@travi.org>
2022-07-07 23:52:27 -0500

    refactor(scripts): pulled up the script that didnt need to be wrapped

--- src/scaffolder-test.js
+++ src/scaffolder-test.js
@@ -35,8 +35,7 @@ suite('cucumber scaffolder', () => {
         scripts: {
           'lint:gherkin': 'gherkin-lint --config=.gherkin-lintrc.json',
           'test:integration': 'run-s \'test:integration:base -- --profile noWip\'',
-          'test:integration:base':
-          'NODE_OPTIONS=--enable-source-maps DEBUG=any cucumber-js test/integration',
+          'test:integration:base': 'NODE_OPTIONS=--enable-source-maps DEBUG=any cucumber-js test/integration',
           'test:integration:debug': 'DEBUG=test run-s test:integration',
           'test:integration:wip': 'run-s \'test:integration:base -- --profile wip\'',
           'test:integration:wip:debug': 'DEBUG=test run-s \'test:integration:wip\'',
--- test/integration/features/step_defintions/scripts-steps.mjs
+++ test/integration/features/step_defintions/scripts-steps.mjs
@@ -9,8 +9,7 @@ Then('the npm scripts are defined', async function () {
     Object.entries({
       'lint:gherkin': 'gherkin-lint --config=.gherkin-lintrc.json',
       'test:integration': 'run-s \'test:integration:base -- --profile noWip\'',
-      'test:integration:base':
-      'NODE_OPTIONS=--enable-source-maps DEBUG=any cucumber-js test/integration',
+      'test:integration:base': 'NODE_OPTIONS=--enable-source-maps DEBUG=any cucumber-js test/integration',
       'test:integration:debug': 'DEBUG=test run-s test:integration',
       'test:integration:wip': 'run-s \'test:integration:base -- --profile wip\'',
       'test:integration:wip:debug': 'DEBUG=test run-s \'test:integration:wip\'',
